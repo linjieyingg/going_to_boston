392d4f762509b8ffdc7d64b1e50f7278b8c731e1
Jieying Lin <jlin41@stuy.edu>
2023-11-09 14:50:39 -0500

    s

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict"
+let rounds = 0
+
+document.querySelector(".submit").addEventListener("click", function () {
+    console.log("Hi")
+    rounds = document.getElementById("rounds").value;
+    // console.log(rounds)
+    });
